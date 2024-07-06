63ec8d0b2d04beaa9452bd7d0f45a60e03d3a709
John Samuelson <john.samuelson@scatterspoke.com>
2024-07-06 09:22:03 -0600

    dev: bootstrap project with sprint boot

---
+++ src/test/java/com/js0724/tool_rental/ToolRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js0724.tool_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
